dd10479a1e94b9093b97c9f68104d98c0fdf3703
freakin23 <rameezparwez@gmail.com>
2023-07-10 14:11:07 +0530

    Commit Changes

--- DP/Knapsack.cpp
+++ DP/Knapsack.cpp
@@ -3,35 +3,38 @@ using namespace std;
 
 int main(){
     
-    ios_base::sync_with_stdio(false);
+    ios_base :: sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(0);
-    cin.exceptions(ios::badbit|ios::failbit);
+    cin.exceptions(ios :: badbit | ios :: failbit);
 
-    auto Knapsack=[&](){
-        int n,x;
-        cin>>n>>x;
-        vector<int> arr(n),price(n);
-        vector<vector<int>> dp(n+4,vector<int>(x+4,0));
+    auto Knapsack = [&] () {
+        int n, x;
+        cin >> n >> x;
+        vector<int> Arr(n), price(n);
 
-        for(auto &x:arr){
-            cin>>x;
-        }
-        for(auto &x:price){
-            cin>>x;
+        for (auto &x : Arr) {
+            cin >> x;
         }
 
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=x;j++){
-                dp[i][j]=dp[i-1][j];
-                int left=j-arr[i-1];
-                if(left>=0){
-                    dp[i][j]=max(dp[i][j],dp[i-1][left]+price[i-1]);
+        for (auto &x : price) {
+            cin >> x;
+        }
+
+        vector<vector<int>> dp(n + 4, vector<int> (x + 4, 0));
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= x; j++) {
+                dp[i][j] = dp[i - 1][j];
+                int left = j - Arr[i - 1];
+                if (left >= 0) {
+                    dp[i][j] = max(dp[i][j], dp[i - 1][left] + price[i - 1]);
                 }
             }
         }
 
-        cout<<(int)dp[n][x]<<'\n';
+        cout << (int)dp[n][x] << '\n';
+
     };Knapsack();
 
     return 0;
