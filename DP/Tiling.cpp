d5951b3c1e7253968e057e43b8745d1e205e839a
freakin23 <rameezparwez@gmail.com>
2023-07-11 17:41:50 +0530

    Commit Changes

--- DP/Tiling.cpp
+++ DP/Tiling.cpp
@@ -1,27 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     
-    ios_base::sync_with_stdio(false);
+    ios_base :: sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(0);
-    cin.exceptions(ios::badbit|ios::failbit);
+    cin.exceptions(ios :: badbit | ios :: failbit);
 
-    auto Tiling=[&](){  //3xn board with 2x1 Dominoes
+    auto Tiling = [&] () { // 3 x n Board with 2 x n Dominoes
         int n;
-        cin>>n;
+        cin >> n;
+        vector<int> a(n + 1), b(n + 1);
+        a[0] = 1, a[1] = 0;
+        b[0] = 0, b[1] = 1;
 
-        vector<int> a(n+1),b(n+1);
-        a[0]=1,a[1]=0,b[0]=0,b[1]=1;
-
-        for(int i=2;i<=n;i++){
-            a[i]=a[i-2]+2*b[i-1];
-            b[i]=a[i-1]+b[i-2];
+        for (int i = 2; i <= n; i++) {
+            a[i] = a[i - 2] + 2 * b[i - 1];
+            b[i] = a[i - 1] + b[i - 2];
         }
 
-        cout<<(int)a[n]<<'\n';
+        cout << a[n] << '\n';
     };Tiling();
-
+    
     return 0;
 }
