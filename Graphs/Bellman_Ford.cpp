bdee1fdda9327b33b001ebfc0d00b9b7cb16696f
freakin23 <rameezparwez@gmail.com>
2023-07-13 01:55:08 +0530

    Commit Changes

--- Graphs/Bellman_Ford.cpp
+++ Graphs/Bellman_Ford.cpp
@@ -1,38 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
+int main() {
+
+    ios_base :: sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(0);
-    cin.exceptions(ios::badbit|ios::failbit);
+    cin.exceptions(ios :: badbit | ios :: failbit);
 
-    auto Bellman_Ford=[&](){
-        int node,edges;
-        cin>>node>>edges;
-        vector<tuple<int,int,int>> Graphs;
-        for(int i=1;i<=edges;i++){
-            int u,v,w;
-            cin>>u>>v>>w;
-            Graphs.push_back(make_tuple(u,v,w));
+    auto Bellman_Ford = [&] () -> void {
+
+        int nodes, edges;
+        cin >> nodes >> edges;
+        vector<tuple<int, int, int>> G;
+        vector<int> dis(nodes + 4, INT_MAX);
+        for (int i = 1; i <= edges; i++) {
+            int a, b, w;
+            cin >> a >> b >> w;
+            G.push_back(make_tuple(a, b, w));
         }
-        vector<int> dst(node+1,INT_MAX);
-        int from;
-        cin>>from;
-        dst[from]=0;
 
-        for(int i=1;i<node;i++){
-            for(auto x:Graphs){
-                int a,b,w;
-                tie(a,b,w)=x;
-                dst[b]=min(dst[b],dst[a]+w);
+        int from;
+        cin >> from;
+        dis[from] = 0;
+
+        for (int i = 1; i + 1 < nodes; i++) {
+            for (auto u : G) {
+                int a, b, w;
+                tie(a, b, w) = u;
+                dis[b] = min(dis[b], dis[a] + w);
             }
         }
 
-        for(int i=1;i<=node;i++){
-            cout<<dst[i]<<" \n"[i==node];
+        for (int i = 1; i <= nodes; i++) {
+            cout << dis[i] << " \n"[i == nodes];
         }
-    };Bellman_Ford();
 
+    };Bellman_Ford();
+    
     return 0;
 }
