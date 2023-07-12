53d1ac77b1f29c848f5103316b68e59f38043dd3
freakin23 <rameezparwez@gmail.com>
2023-07-13 01:24:41 +0530

    Commit Changes

--- Graphs/CONNECTED_COMPS.cpp
+++ Graphs/CONNECTED_COMPS.cpp
@@ -1,48 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
+int main() {
+
+    ios_base :: sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(0);
-    cin.exceptions(ios::badbit|ios::failbit);
+    cin.exceptions(ios :: badbit | ios :: failbit);
 
-    auto CONNECTED_COMPS=[&](){
-        int node,edges;
-        cin>>node>>edges;
-        vector<int> adj[node+4];
-        vector<bool> used(node+4);
-        vector<vector<int>> connect_comps;
-        for(int i=1;i<=edges;i++){
-            int u,v;
-            cin>>u>>v;
+    auto Connected_Componets = [&] () {
+
+        const int N = 4E4 + 4;
+        vector<int> adj[N];
+        vector<bool> used(N, false);
+        vector<vector<int>> comps;
+
+        int nodes, edges;
+        cin >> nodes >> edges;
+        for (int i = 1; i <= edges; i++) {
+            int u, v;
+            cin >> u >> v;
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
 
-        function<void(int,vector<int>&)> Dfs=[&](int j,vector<int> &connect){
-            used[j]=true;
-            connect.push_back(j);
-            for(auto u:adj[j]){
-                if(!used[u]){
-                    Dfs(u,connect);
+        function<void(vector<int> &, int)> Dfs = [&] (vector<int> &c, int j) {
+            used[j] = true;
+            c.push_back(j);
+            for (auto u : adj[j]) {
+                if (!used[u]) {
+                    Dfs(c, u);
                 }
             }
         };
 
-        for(int i=1;i<=node;i++){
-            if(!used[i]){
-                vector<int> connect;
-                Dfs(i,connect);
-                connect_comps.push_back(connect);
+        for (int i = 1; i <= nodes; i++) {
+            if (!used[i]) {
+                vector<int> c;
+                Dfs(c, i);
+                comps.push_back(c);
+                c.clear();
             }
         }
-        for(int i=0;i<(int)connect_comps.size();i++){
-            for(int j=0;j<(int)connect_comps[i].size();j++){
-                cout<<connect_comps[i][j]<<" \n"[j==(int)connect_comps[i].size()-1];
+
+        cout << (int)comps.size() << '\n';
+        for (auto x : comps) {
+            for (auto y : x) {
+                cout << y << " ";
             }
+            cout << '\n';
         }
-    };CONNECTED_COMPS();
+    };Connected_Componets();
 
     return 0;
 }
