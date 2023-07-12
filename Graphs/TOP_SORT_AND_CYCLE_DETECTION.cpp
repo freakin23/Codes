aae094e26d0a03a850dce658afc82bf85de462be
freakin23 <rameezparwez@gmail.com>
2023-07-13 01:43:15 +0530

    Commit Changes

--- Graphs/TOP_SORT_AND_CYCLE_DETECTION.cpp
+++ Graphs/TOP_SORT_AND_CYCLE_DETECTION.cpp
@@ -1,52 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
+int main() {
+
+    ios_base :: sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(0);
-    cin.exceptions(ios::badbit|ios::failbit);
+    cin.exceptions(ios :: badbit | ios :: failbit);
 
-    auto TOP_SORT_AND_CYCLE_DETECTION=[&](){
-        int node,edges;
-        cin>>node>>edges;
-        vector<int> adj[node+4],top_sort;
-        vector<bool> used1(node+4),used2(node+4);
+    auto Top_Sort_and_Cycle_Detection = [&] () {
 
-        for(int i=1;i<=edges;i++){
-            int u,v;
-            cin>>u>>v;
+        const int N = 4E4 + 4;
+        vector<int> adj[N];
+        vector<int> used1(N, false), used2(N, false);
+        vector<int> top_sort;
+
+        int nodes, edges;
+        cin >> nodes >> edges;
+        for (int i = 1; i <= edges; i++) {
+            int u, v;
+            cin >> u >> v;
             adj[u].push_back(v);
-            adj[v].push_back(u);
         }
 
-        function<void(int)> Dfs=[&](int j){
-            used1[j]=true;
-            used2[j]=true;
+        auto Dfs = [&] (auto Dfs, int j) {
+            
+            if (used1[j]) {
+                return;
+            }
 
-            for(auto u:adj[j]){
-                if(!used1[u] and !used2[u]){
-                    Dfs(u);
+            used1[j] = used2[j] = true;
+            for (auto u : adj[j]) {
+                if (!used1[u] and !used2[u]) {
+                    Dfs(Dfs, u);
                 }
-                else if(used2[u]){
-                    cout<<"Found the cycle"<<'\n';
+                else if (used2[u]) {
+                    cout << "Found the Cycle" << '\n';
                     exit(0);
                 }
             }
-            used2[j]=false;
+
+            used2[j] = false;
             top_sort.push_back(j);
         };
-        for(int i=1;i<=node;i++){
-            if(!used1[i]){
-                Dfs(i);
+
+        for (int i = 1; i <= nodes; i++) {
+            if (!used1[i]) {
+                Dfs(Dfs, i);
             }
         }
 
-        for(int i=1;i<(int)top_sort.size();i++){
-            cout<<top_sort[i]<<" \n"[i==(int)top_sort.size()-1];
+        reverse(begin(top_sort), end(top_sort));
+        for (int i = 0; i < (int)top_sort.size(); i++) {
+            cout << top_sort[i] << " \n"[i == (int)top_sort.size() - 1];
         }
 
-    };TOP_SORT_AND_CYCLE_DETECTION();
+    };Top_Sort_and_Cycle_Detection();
 
     return 0;
 }
