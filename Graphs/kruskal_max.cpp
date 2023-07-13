f4a14b5ed06a9fc578e6f81548cc9f32d9f2fa24
freakin23 <rameezparwez@gmail.com>
2023-07-13 17:04:48 +0530

    Commit Changes

--- Graphs/kruskal_max.cpp
+++ Graphs/kruskal_max.cpp
@@ -1,55 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
+int main() {
+
+    ios_base :: sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(0);
-    cin.exceptions(ios::badbit|ios::failbit);
+    cin.exceptions(ios :: badbit | ios :: failbit);
 
-    auto kruskal_max=[&](){
-        int node,edges;
-        cin>>node>>edges;
-        struct Edge{
-            int u,v,weight;
-            bool operator<(Edge const &other){
-                return weight>other.weight;
+    auto Kruskal_max = [&] () -> void {
+
+        struct Edge {
+            int u, v, weight;
+            bool operator < (Edge const &other) {
+                return weight > other.weight;
             }
         };
 
-        vector<Edge> tree,MST;
-        int cost=0;
-        for(int i=1;i<=edges;i++){
-            int a,b,c;
-            cin>>a>>b>>c;
-            tree.push_back({a,b,c});
-            tree.push_back({b,a,c});
+        vector<Edge> tree, MST;
+        int cost = 0, nodes, edges;
+        cin >> nodes >> edges;
+        for (int i = 1; i <= edges; i++) {
+            int a, b, w;
+            cin >> a >> b >> w;
+            tree.push_back({a, b, w});
+            tree.push_back({b, a, w});
         }
 
-        vector<int> tree_id(node+4);
-        iota(tree_id.begin(),tree_id.end(),0);
-        sort(tree.begin(),tree.end());
-        
-        for(auto e:tree){
-            if(tree_id[e.u]!=tree_id[e.v]){
-                cost+=e.weight;
+        sort(begin(tree), end(tree));
+        vector<int> tree_id(nodes + 4, 0);
+        iota(begin(tree_id), end(tree_id), 0);
+
+        for (auto e : tree) {
+            if (tree_id[e.u] != tree_id[e.v]) {
+                cost += e.weight;
                 MST.push_back(e);
 
-                int old_id=tree_id[e.u],new_id=tree_id[e.v];
-                for(int i=1;i<=node;i++){
-                    if(tree_id[i]==old_id){
-                        tree_id[i]=new_id;
+                int old_id = tree_id[e.u], new_id = tree_id[e.v];
+                for (int i = 1; i <= nodes; i++) {
+                    if (tree_id[i] == old_id) {
+                        tree_id[i] = new_id;
                     }
                 }
             }
         }
-        cout<<"Cost is: "<<cost<<'\n';
-        cout<<"Edge"<<setw(8)<<"Weight"<<'\n';
-        for(auto x:MST){
-            cout<<x.u<<'-'<<x.v<<setw(6)<<x.weight<<'\n';
-        }
 
-    };kruskal_max();
+        cout << "Cost: " << cost << '\n';
+        cout << "Edge" << setw(8) << "Weight" << '\n';
+        for (auto e : MST) {
+            cout << e.u << '-' << e.v << setw(6) << e.weight << '\n';
+        }
+        
+    };Kruskal_max();
 
     return 0;
 }
