4f4f8a9a75e00e8543d6bb165a5cede9815936d2
freakin23 <rameezparwez@gmail.com>
2023-07-13 13:05:11 +0530

    Commit Changes

--- Graphs/Floyd_Warshall.cpp
+++ Graphs/Floyd_Warshall.cpp
@@ -1,42 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
+int main() {
+
+    ios_base :: sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(0);
-    cin.exceptions(ios::badbit|ios::failbit);
+    cin.exceptions(ios :: badbit | ios :: failbit);
 
-    auto Floyd_Warshall=[&](){
-        int F_node,F_edges;
-        cin>>F_node>>F_edges;
+    auto Floyd_Warshall = [&] () -> void {
 
-        vector<vector<int>> Floyd(F_node+4,vector<int>(F_node+4,INT_MAX));
-        for(int i=1;i<=F_node;i++){
-            Floyd[i][i]=0;
+        int nodes, edges;
+        cin >> nodes >> edges;
+        vector<vector<long>> adj(nodes + 1, vector<long> (nodes + 1, INT_MAX));
+
+        for (int i = 1; i <= nodes; i++) {
+            adj[i][i] = 0;
+        }
+        for (int i = 1; i <= edges; i++) {
+            long a, b, w;
+            cin >> a >> b >> w;
+            adj[a][b] = min(adj[a][b], w);
+            adj[b][a] = min(adj[b][a], w);
         }
 
-        for(int i=1;i<=F_edges;i++){
-            int a,b,c;
-            cin>>a>>b>>c;
-            Floyd[a][b]=min(Floyd[a][b],c);
-            Floyd[b][a]=min(Floyd[b][a],c);
-        }
-
-        for(int k=1;k<=F_node;k++){
-            for(int i=1;i<=F_node;i++){
-                for(int j=1;j<=F_node;j++){
-                    Floyd[i][j]=min(Floyd[i][j],Floyd[i][k]+Floyd[k][j]);
+        for (int k = 1; k <= nodes; k++) {
+            for (int i = 1; i <= nodes; i++) {
+                for (int j = 1; j <= nodes; j++) {
+                    adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
                 }
             }
         }
 
-        for(int i=1;i<=F_node;i++){
-            for(int j=1;j<=F_node;j++){
-                cout<<Floyd[i][j]<<" ";
+        for (int i = 1; i <= nodes; i++) {
+            for (int j = 1; j <= nodes; j++) {
+                cout << adj[i][j] << " \n"[j == nodes];
             }
-            cout<<'\n';
         }
+
     };Floyd_Warshall();
 
     return 0;
