995434f27e4fe4dbb48814a949a0c976c1cafd5f
freakin23 <rameezparwez@gmail.com>
2024-09-04 21:23:44 +0530

    phi

--- J/euler_totient.cpp
+++ J/euler_totient.cpp
@@ -3,6 +3,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int phi(int n) {
+    int res = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0) {
+                n /= i;
+            }
+            res -= res / i;
+        }
+    }
+    if (n > 1) {
+        res -= res / n;
+    }
+    return res;
+}
 
+int main() {
+    cout << phi(6) << '\n'; //  2
+    cout << phi(12) << '\n'; //  4
+    cout << phi(15) << '\n'; //  8
 }
