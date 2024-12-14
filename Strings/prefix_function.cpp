b16860ec446efbf7c0c38bb8e37e34718bcfe588
freakin23 <rameezparwez@gmail.com>
2024-12-14 20:08:51 +0530

    update prefix_function

--- Strings/prefix_function.cpp
+++ Strings/prefix_function.cpp
@@ -3,15 +3,22 @@
 class Prefix_Function {
     public:
     std::vector<int> pi;
-    std::string s;
-    int n;
-    Prefix_Function(std::string s) {
+    std::vector<int> occurence;
+    std::string s, t;
+    int n, pat_sz = 0, f = 0;
+    Prefix_Function(std::string const& s, std::string const& t = "") {
+        f = (int)t.size();
         this->s = s;
-        this->n = (int)s.size();
+        if (f) {
+            pat_sz = (int)s.size();
+            this->t = t;
+        }
+        this->n = (f ? (int)t.size() + (int)s.size() + 1 : (int)s.size());
         pi = std::vector<int> (n, 0);
     }
 
-    void prefix_function() {
+    void prefix_function(std::string const& s) {
+        int n = (int)s.size();
         for (int i = 1; i < n; i++) {
             int j = pi[i - 1];
             while (j > 0 and s[i] != s[j]) {
@@ -22,10 +29,19 @@ class Prefix_Function {
             }
             pi[i] = j;
         }
+
+        if (f) {
+            for (int i = 0; i < n; i++) {
+                if (pi[i] == pat_sz) {
+                    occurence.push_back(i - 2 * pat_sz + 1);
+                }
+            }
+        }
     }
 
     void build() {
-        prefix_function();
+        s = (f ? s + '#' + t : s);
+        prefix_function(s);
     }
 };
 
@@ -46,17 +62,18 @@ std::vector<int> prefix_function(std::string const& s) {
 }
 
 int main() {
-    std::string s;
-    std::cin >> s;
-    int n = (int)s.size();
-    std::vector<int> pi = prefix_function(s);
-    for (int i = 0; i < n; i++) {
-        std::cout << pi[i] << " \n"[i == n - 1];
+    std::string s, t;
+    std::cin >> s >> t;
+    Prefix_Function pf = Prefix_Function(s, t);
+    pf.build();
+    for (int i = 0; i < (int)pf.occurence.size(); i++) {
+        std::cout << pf.occurence[i] << " \n"[i == (int)pf.occurence.size() - 1];
     }
 
-    Prefix_Function pf = Prefix_Function(s);
-    pf.build();
-    for (int i = 0; i < n; i++) {
-        std::cout << pf.pi[i] << " \n"[i == n - 1];
+    std::string a = "aabaaab";
+    Prefix_Function A = Prefix_Function(a);
+    A.build();
+    for (int i = 0; i < (int)A.pi.size(); i++) {
+        std::cout << A.pi[i] << " \n"[i == (int)A.pi.size() - 1];
     }
 }
