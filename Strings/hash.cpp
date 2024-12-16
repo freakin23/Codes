8970567cd4108febfd1eca11157771984e322488
freakin23 <rameezparwez@gmail.com>
2024-12-16 13:05:05 +0530

    update hash.cpp

--- Strings/hash.cpp
+++ Strings/hash.cpp
@@ -29,3 +29,32 @@ std::vector<std::vector<int>> group_identical_strings(std::vector<std::string> a
 
 	return groups;
 }
+
+int count_unique_substrings(std::string const& s) {
+    int n = (int)s.size();
+
+    const int p = 31;
+    const int m = 1e9 + 9;
+    std::vector<long long> p_pow(n);
+    p_pow[0] = 1;
+    for (int i = 1; i < n; i++) {
+        p_pow[i] = (p_pow[i - 1] * p) % m;
+    }
+
+    std::vector<long long> h(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+        h[i + 1] = (h[i] + (s[i] - 'a' + 1) * p_pow[i]) % m;
+    }
+
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        std::unordered_set<long long> hs;
+        for (int j = 0; j <= n - i; j++) {
+            long long curr_h = (h[j + 1] + m - h[j]) % m;
+            curr_h = (curr_h * p_pow[n - j - 1]) % m;
+            hs.insert(curr_h);
+        }
+        cnt += (int)hs.size();
+    }
+    return cnt;
+}
